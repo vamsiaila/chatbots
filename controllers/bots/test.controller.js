30a9b008976b7bf97d280d9a52aaacab6fe03868
vamsi <avamsi.web@gmail.com>
2019-01-01 18:23:52 +0530

    testing

--- controllers/bots/test.controller.js
+++ controllers/bots/test.controller.js
@@ -1,7 +1,6 @@
 
 module.exports = {
     sample : (req,res)=>{
-        req.body.queryResult.fulfillmentMessages[0].text.text[0] = "something for test";
-        res.send(req.body);
+        res.send({speech:'something', displayText:'something', source:'testme'});
     }
 }
