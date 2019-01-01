ae328488eaef1b2cf9fb4250a9e0c6a82ecebf86
vamsi <avamsi.web@gmail.com>
2019-01-01 17:27:11 +0530

    added route

--- routes/bot.js
+++ routes/bot.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const test = require('../controllers/bots/test.controller');
 
-
+router.use('/',(req,res)=>{
+    if(req.queryResult.intent.displayName === 'testing') return test.sample;
+})
 
 module.exports = router;
