f905d83d5e65db4674e164b5ef7078b8631c86d9
vamsi <avamsi.web@gmail.com>
2019-01-01 18:02:10 +0530

    testing

--- routes/bot.js
+++ routes/bot.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const test = require('../controllers/bots/test.controller');
 
 router.post('/',(req,res)=>{
-    console.log(req.body);
     let intent = req.body.queryResult && req.body.queryResult.intent ? req.body.queryResult.intent : '';
+    console.log(intent);
     if(intent.displayName === 'testing') return test.sample(req,res);
     else res.send('invalid intent');
 })
