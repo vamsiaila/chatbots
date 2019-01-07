c5a908873a3fc2ec856f0b61fc3c51080145cc78
vamsi <avamsi.web@gmail.com>
2019-01-07 15:21:07 +0530

    testing

--- controllers/bots/test.controller.js
+++ controllers/bots/test.controller.js
@@ -3,9 +3,6 @@ module.exports = {
     sample : (req,res)=>{
         const payload = {
                 fulfillmentText: "Hi I am Vamsi",
-                payload: {
-                    fulfillmentText: "Hi I am Vamsi"
-                },
             };
         res.send(payload);
     },
@@ -14,16 +11,11 @@ module.exports = {
         const payload = {};
         let query = req.body.queryResult.queryText.toLowerCase();
         if(query.indexOf('free')){
-            payload.payload = {
-                fulfillmentText: "Hi I am free"
-            };
+            payload.fulfillmentText= "Hi I am free"
         }
         else{
-            payload.payload = {
-                fulfillmentText: "Hi I am busy"
-            };
+            payload.fulfillmentText= "Hi I am busy"
         }
-
         res.send(payload)
     }
 }
