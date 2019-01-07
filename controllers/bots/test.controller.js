0240c3d3e67bcb6985c89615af5db51792c8b2e3
vamsi <avamsi.web@gmail.com>
2019-01-07 15:12:25 +0530

    testing

--- controllers/bots/test.controller.js
+++ controllers/bots/test.controller.js
@@ -1,23 +1,28 @@
 
 module.exports = {
     sample : (req,res)=>{
-        
         const payload = {
                 payload: {
-                    google: {
-                        expectUserResponse: true,
-                        richResponse: {
-                            items: [
-                                {
-                                    simpleResponse: {
-                                        ssml: 'hi i am vamsi',
-                                    }
-                                }
-                            ]
-                        },
-                    },
+                    fulfillmentText: "Hi I am Vamsi"
                 },
             };
         res.send(payload);
+    },
+
+    freeTime : (req,res)=>{
+        const payload = {};
+        let query = req.body.queryResult.queryText.toLowerCase();
+        if(query.indexOf('free')){
+            payload.payload = {
+                fulfillmentText: "Hi I am free"
+            };
+        }
+        else{
+            payload.payload = {
+                fulfillmentText: "Hi I am busy"
+            };
+        }
+
+        res.send(payload)
     }
 }
--- routes/bot.js
+++ routes/bot.js
@@ -3,8 +3,9 @@ const test = require('../controllers/bots/test.controller');
 
 router.post('/',(req,res)=>{
     let intent = req.body.queryResult && req.body.queryResult.intent ? req.body.queryResult.intent : '';
-    console.log(intent);
-    if(intent.displayName === 'testing') return test.sample(req,res);
+    let intentName = intent.displayName;
+    if(intentName === 'testing') return test.sample(req,res);
+    if(intentName === 'free_time') return test.freeTime(req,res);
     else res.send('invalid intent');
 })
 
