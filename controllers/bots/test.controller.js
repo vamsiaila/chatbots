ccb28db0d36e3f32aa978f7680b56be8181952ac
vamsi <avamsi.web@gmail.com>
2019-01-07 15:25:29 +0530

    testing

--- controllers/bots/test.controller.js
+++ controllers/bots/test.controller.js
@@ -11,10 +11,10 @@ module.exports = {
         const payload = {};
         let query = req.body.queryResult.queryText.toLowerCase();
         if(query.indexOf('free') > -1){
-            payload.fulfillmentText= "Hi I am free"
+            payload.fulfillmentText= "I am free on Saturday and Sunday"
         }
         else{
-            payload.fulfillmentText= "Hi I am busy"
+            payload.fulfillmentText= "My working hours are/n mon-fri 10:00 - 19:00"
         }
         res.send(payload)
     }
